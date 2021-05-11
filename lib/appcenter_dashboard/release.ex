f2cd0f2c272c59ce7fedb20a9a377fbcaf8c7e61
Blake Kostner <git@btkostner.io>
2021-05-11 16:48:11 -0600

    add release functions

---
+++ lib/appcenter_dashboard/release.ex
@@ -0,0 +1,18 @@
+defmodule Elementary.AppcenterDashboard.Release do
+  @app :appcenter_dashboard
+
+  def migrate do
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+end
